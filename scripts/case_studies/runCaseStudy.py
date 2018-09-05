6818c66184c2952e1d43406c815080b0f8b9a1eb
Saachi Jain <saachi@fb.com>
2018-09-05 10:10:36 -0400

    name updates

--- scripts/case_studies/runCaseStudy.py
+++ scripts/case_studies/runCaseStudy.py
@@ -1,4 +1,4 @@
-from TICC_solver import TICCSolver
+from CASC_solver import CASCSolver
 import numpy as np
 import sys
 import pickle
@@ -12,7 +12,7 @@ def dataset(mode, input_name, output_dir):
     if mode == 1:
         outputName = "%s/old/assign.out" % output_dir
     else:
-        outputName, bic = runNonMotifTICC(input_name, output_dir, number_of_clusters, beta, None)
+        outputName, bic = runNonMotifCASC(input_name, output_dir, number_of_clusters, beta, None)
         print(bic)
     if mode == 1: runHyperParameterTests(input_name, output_dir, number_of_clusters, beta, outputName)
 
@@ -30,14 +30,14 @@ def runBICTests(inputName, number_of_clusters):
     beta = [10, 25, 50]
     bicBeta = []
     for b in beta:
-        _, bic = runNonMotifTICC(inputName, None, number_of_clusters, b, None, numIters=10)
+        _, bic = runNonMotifCASC(inputName, None, number_of_clusters, b, None, numIters=10)
         bicBeta.append((bic, b))
     bicBeta.sort(reverse=True)
     print(bicBeta)
 
 
-def runNonMotifTICC(inputName, outputDir, clusters, beta, oldAssignmentsName, numIters=15):
-    if outputDir is not None: 
+def runNonMotifCASC(inputName, outputDir, clusters, beta, oldAssignmentsName, numIters=15):
+    if outputDir is not None:
         oldDir = "%s/old/" % outputDir
         makeDir(oldDir)
         outputDir = oldDir
@@ -56,14 +56,14 @@ def makeDir(dirname):
 def runTest(mode, inputName, outputDir, clusters, beta, gamma, motifReq, oldAssignmentsName, maxIters):
     print("TESTING %s" % (gamma))
     #maxIters used to be 30
-    solver = TICCSolver(window_size=1, number_of_clusters=clusters, lambda_parameter=1e-3, beta=beta, threshold=2e-5,
+    solver = CASCSolver(window_size=1, number_of_clusters=clusters, lambda_parameter=1e-3, beta=beta, threshold=2e-5,
                         gamma=gamma, input_file=inputName, num_proc=10, maxMotifs=50, motifReq=motifReq, maxIters=maxIters)
     old_assign = None
     usemotif = False
     if mode == 1:
         old_assign = np.loadtxt(oldAssignmentsName, dtype=int)
         usemotif = True
-    (cluster_assignment, cluster_MRFs, motifs, motifRanked, bic, _) = solver.PerformFullTICC(
+    (cluster_assignment, cluster_MRFs, motifs, motifRanked, bic, _) = solver.PerformFullCASC(
         initialClusteredPoints=old_assign, useMotif=usemotif)
     solver.CleanUp()
     if usemotif and outputDir is not None:
@@ -84,11 +84,10 @@ if __name__ == "__main__":
     # mode of 1 to skip old assign
     assert len(sys.argv) > 1
     mode = int(sys.argv[1])
-    if mode == 2: 
+    if mode == 2:
         input_name = sys.argv[2]
         runBICTests(input_name, CLUSTER_NUMBER)
     else:
         assert len(sys.argv) == 4
         mode, input_fname, output_fdir = int(sys.argv[1]), sys.argv[2], sys.argv[3]
         dataset(mode, input_fname, output_fdir)
-
